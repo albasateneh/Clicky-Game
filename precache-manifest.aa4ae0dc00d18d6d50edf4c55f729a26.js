fd03507b13d28ee8fbe6f1bf61ee8395d981dec9
sam <sam.albasateneh@gmail.com>
2019-04-01 13:08:46 -0700

    Updates

---
+++ precache-manifest.aa4ae0dc00d18d6d50edf4c55f729a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fffda41e55107e1b2f4",
+    "url": "/Clicky-Game/static/css/main.383fe0fd.chunk.css"
+  },
+  {
+    "revision": "1fffda41e55107e1b2f4",
+    "url": "/Clicky-Game/static/js/main.c907dda3.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "ac028754161e1120cb4f",
+    "url": "/Clicky-Game/static/css/2.d79eb21a.chunk.css"
+  },
+  {
+    "revision": "ac028754161e1120cb4f",
+    "url": "/Clicky-Game/static/js/2.2aca4f66.chunk.js"
+  },
+  {
+    "revision": "087320a496d4c05e894edaa69f2c9d2a",
+    "url": "/Clicky-Game/index.html"
+  }
+];
